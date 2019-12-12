fc48063db88ed3da7066e03f64176b65c4dc1232
jayhem10 <57893801+jayhem10@users.noreply.github.com>
2019-12-12 16:50:42 +0100

    maj

--- src/model/user.php
+++ src/model/user.php
@@ -166,5 +166,55 @@ class User extends Db {
             }
         
         }
+
+        public function signin() {
+            
+            //$nom = User::signin();
+    
+            $form = new Form($_POST);
+    
+            $form->input("select", 'civilite', 'Civilité', [1=>'M', 2=>'Mme', 3=>'Mlle'])->required()
+                ->input('text', "nom", "Nom")->required()
+                ->input('text', "prenom", "Prénom")->required()
+                ->input('text', "email", "E-mail")->required()
+                ->input('password', 'password', 'Mot de passe')->required()
+                ->input('password', 'password2', 'Confirmer mot de passe')->required()
+                ->submit('S\'inscrire');
+    
+            $formulaireHtml1 = $form->getForm();
+    
+            $formValid  = false;
+            $errors     = false; 
+    
+            // si le formulaire est validé 
+            if (!empty($_POST)) {
+                
+                if($form->valid()){
+    
+                    // formulaire valide
+                    $formValid = true;
+                
+                    // Enregistrement des données
+                    // insertion des données via le model Userbase 
+                    $id = User::signin([
+                        "usr_nom"     => $_POST['nom'],
+                        "usr_slug"    => slugify($_POST['nom']),
+                        "usr_prenom"   => $_POST['prenom'],
+                        "usr_email"   => $_POST['email']
+                    ]);
+    
+                    // redirection apres ajout en BDD 
+                    redirectTo('user/'.$id.'/'.slugify($_POST['nom']));
+    
+                } else {
+                    // affichage des erreurs 
+                    $errors =  $form->displayErrors();
+                }
+            }
+    
+            view('pages.login', compact('formulaireHtml', 'errors', 'formValid'));
+        }
+
+        
  
 } 
