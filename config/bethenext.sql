febfdc1faf9715447708a25508ca0c689a21b39e
YassineFassi <yassine.fassi@gmail.com>
2019-12-16 11:35:36 +0100

    maj BDD

--- config/bethenext.sql
+++ config/bethenext.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 16 déc. 2019 à 10:36
+-- Généré le :  lun. 16 déc. 2019 à 11:33
 -- Version du serveur :  10.1.31-MariaDB
 -- Version de PHP :  7.2.4
 
@@ -62,9 +62,9 @@ CREATE TABLE `player` (
   `p_prenom` text NOT NULL,
   `p_birth` date NOT NULL,
   `p_club` text NOT NULL,
-  `p_img` text NOT NULL,
-  `p_full` varchar(255) NOT NULL,
-  `p_hover` varchar(500) NOT NULL,
+  `p_img` text NOT NULL COMMENT 'affiche sur les cards de la page user',
+  `p_img_hover_l` varchar(255) NOT NULL COMMENT 'affiche dans sur la page du joueur (format PNG)',
+  `p_img_hovers` varchar(500) NOT NULL COMMENT 'affiche sur la page TOUS les joueurs (format 1080)',
   `p_id` int(11) NOT NULL,
   `s_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -73,19 +73,19 @@ CREATE TABLE `player` (
 -- Déchargement des données de la table `player`
 --
 
-INSERT INTO `player` (`p_nom`, `p_prenom`, `p_birth`, `p_club`, `p_img`, `p_full`, `p_hover`, `p_id`, `s_id`) VALUES
+INSERT INTO `player` (`p_nom`, `p_prenom`, `p_birth`, `p_club`, `p_img`, `p_img_hover_l`, `p_img_hovers`, `p_id`, `s_id`) VALUES
 ('Cristiano', 'Ronaldo', '1985-02-05', 'Juventus Football Club', 'https://media2.woopic.com/api/v1/images/504%2Fmedia365-sport-news%2F2f1%2F409%2F1140a462f378290263df97002b%2Fcristiano-ronaldo-frappe-son-enfant-avec-un-trophee%7C9116488_cristiano-ronaldo-1-1024x425_ext.jpg?format=980x450&facedetect=1&quality=85', 'https://img.pngio.com/cristiano-ronaldo-png-transparent-images-pictures-photos-png-arts-cristiano-ronaldo-pngs-hd-1280_1080.png', 'https://amp.thenational.ae/image/policy:1.801169:1544442749/sp11-Cristiano-Ronaldo.jpg?f=16x9&w=1200&$p$f$w=2a62836', 2, 2),
 ('LeBron', 'James', '1984-12-30', 'Los Angeles Lakers', 'https://cdn.vox-cdn.com/thumbor/G8Vty8xReaUMC3xLti_ysvwDf4g=/0x0:4150x2767/1200x800/filters:focal(1226x679:1890x1343)/cdn.vox-cdn.com/uploads/chorus_image/image/65163856/GettyImages_1094200282.0.jpg', 'https://retohercules.com/images/allen-iverson-png-7.png', 'https://images.radio-canada.ca/q_auto,w_1250/v1/ici-info/sports/16x9/lebron-james-basketball-lakers-los-angeles.jpg', 3, 1),
 ('Da Silva Santos Jr', 'Neymar', '1992-02-05', 'Paris Saint Germain', 'https://footradio.com/wp-content/uploads/2019/09/Icon_BAP_220919_93_75.jpg', 'https://www.freeiconspng.com/uploads/neymar-png-athlete-24.png', 'https://images.daznservices.com/di/library/GOAL/63/1a/neymar-psg-liverpool-uefa-champions-league-28112018_uy6anrxcvr8r12m3bxqs0v4vm.jpg?t=1540878583&quality=100', 5, 2),
-('Harden', 'James', '1989-08-26', 'Houston Rockets', 'http://www.sport24info.ma/wp-content/uploads/2019/03/James-Harden.jpg', 'https://www.stickpng.com/assets/images/58456780869fe441e3fadea7.png', 'https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F191008151032-james-harden.jpg', 6, 1),
-('Brady', 'Tom', '1977-08-03', 'Patriots', 'https://usatftw.files.wordpress.com/2019/11/ap-patriots-eagles-football-4.jpg?w=1000&h=600&crop=1', 'http://pluspng.com/img-png/tom-brady-png-download-1195x1593-png-450.png', 'https://sportshub.cbsistatic.com/i/r/2019/12/09/0b5280c8-1952-414d-adfd-44e035727f64/thumbnail/1200x675/aa2301a2601854d24782ab14c59512ac/tom-brady-patriots.jpg', 7, 4),
-('Mbappé', 'Kylian', '1998-12-20', 'Paris Saint Germain', 'http://www.allpaname.fr/wp-content/uploads/2018/10/i-1.jpg', 'https://paris-supporters.fr/wp-content/uploads/2019/07/mbappe-champion.jpg', 'https://footradio.com/wp-content/uploads/2019/01/Kylian-Mbappe.jpg', 8, 2),
-('Antetokoúnmpo', 'Giannis', '1994-12-06', 'Milwaukee Bucks', 'https://img.bleacherreport.net/img/images/photos/003/830/747/hi-res-9ec52efc79e349bfb3ca48aff3bdc458_crop_north.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSPElh4xGE4nX5EoqGOo0-vJROnSikYvXbWP6LE7kzUpVb2sNMR', 'https://www.parlons-basket.com/wp-content/uploads/2019/12/incroyable-stat-giannis-antetokounmpo.jpg', 10, 1),
-('Rodgers', 'Aaron', '1983-12-02', 'Green Bay Packers', 'https://lasueur.com/wp-content/uploads/2017/12/Aaron-Rodgers.jpg', 'https://usathss.files.wordpress.com/2019/09/xxx-a01-rodgers-e1567705706252.jpg', 'https://skystatement.com/wp-content/uploads/2019/11/44937/aaron-rodgers-is-the-49ers-biggest-mistake.jpeg-26', 11, 4),
-('Gronkowski', 'Rob', '1989-05-14', 'New England Patriots', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Rob_Gronkowski.JPG/800px-Rob_Gronkowski.JPG', 'https://lasueur.com/wp-content/uploads/2018/04/Rob-Gronkowski-NFL-patriots.jpg', 'https://answersafrica.com/wp-content/uploads/2018/09/Rob-Gronk-111-640x421.jpeg', 12, 4),
-('Crosby', 'Sidney ', '1987-08-07', 'Pittsburgh Penguins', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Sidney_Crosby_1_2017-05-25_%28Edited%29.jpg/220px-Sidney_Crosby_1_2017-05-25_%28Edited%29.jpg', 'https://rdsimages.cookieless.ca/polopoly_fs/1.7010825.1573743327!/img/httpImage/image.jpg_gen/derivatives/main-xxxhdpi/image.jpg', 'https://i.ytimg.com/vi/MqQeVzhrS0k/maxresdefault.jpg', 13, 3),
-('Kane', 'Patrick', '1988-11-19', 'Chicago Blackhawks', 'https://img.s-msn.com/tenant/amp/entityid/BBgzGRT.img?x=1&y=1&f=PNG&h=336&w=320&m=5&q=60&b=white', 'https://cdn.vox-cdn.com/thumbor/7iqQmqhyzyhL6FGW2Uv2SZedTyM=/0x0:2308x2668/1200x800/filters:focal(861x663:1229x1031)/cdn.vox-cdn.com/uploads/chorus_image/image/62803675/1076441838.jpg.0.jpg', 'https://blackhawkup.com/wp-content/uploads/usat-images/2017/03/9928675-nhl-anaheim-ducks-at-chicago-blackhawks.jpeg', 14, 3),
-('Ovetchkine ', 'Aleksandr ', '1985-09-17', 'Washington Capitals', 'https://s.yimg.com/ny/api/res/1.2/gdhG3QtFD.rmlV24GkeIiw--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NzQ0O2g9NDk2/http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/bff536b0e7b7527d4f21b3ea4c4f10bb', 'https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/J556GPQSJQI6VESMWNGQTO6JJA.jpg', 'http://images.4ever.eu/data/download/gens/athletes/aleksandr-ovetchkine,-joueur-de-hockey-229488.jpg?no-logo', 15, 3);
+('Harden', 'James', '1989-08-26', 'Houston Rockets', 'http://www.sport24info.ma/wp-content/uploads/2019/03/James-Harden.jpg', 'https://www.stickpng.com/assets/images/5845678a869fe441e3fadea8.png', 'https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F191008151032-james-harden.jpg', 6, 1),
+('Brady', 'Tom', '1977-08-03', 'Patriots', 'https://usatftw.files.wordpress.com/2019/11/ap-patriots-eagles-football-4.jpg?w=1000&h=600&crop=1', 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7827b2c1-5dcb-455b-8653-0e324f8adb1c/ddepocb-d46a15c3-9ffa-4e95-9efe-dd92f5be2c24.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwi', 'https://sportshub.cbsistatic.com/i/r/2019/12/09/0b5280c8-1952-414d-adfd-44e035727f64/thumbnail/1200x675/aa2301a2601854d24782ab14c59512ac/tom-brady-patriots.jpg', 7, 4),
+('Mbappé', 'Kylian', '1998-12-20', 'Paris Saint Germain', 'https://paris-supporters.fr/wp-content/uploads/2019/07/mbappe-champion.jpg', 'https://images.psg.media/media/27066/profile-7-mbappe.png?anchor=center&mode=crop&width=800&height=800&quality=80', 'https://www.typeria.pl/wp-content/uploads/2019/02/kylian-mbappe-cropped_t7cjy8h52cts1d5mugi4u6my9.jpg', 8, 2),
+('Antetokoúnmpo', 'Giannis', '1994-12-06', 'Milwaukee Bucks', 'https://img.bleacherreport.net/img/images/photos/003/830/747/hi-res-9ec52efc79e349bfb3ca48aff3bdc458_crop_north.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top', 'https://www.trzcacak.rs/myfile/full/97-979010_giannis-antetokounmpo-giannis-antetokounmpo-no-background.png', 'https://www.parlons-basket.com/wp-content/uploads/2019/12/incroyable-stat-giannis-antetokounmpo.jpg', 10, 1),
+('Rodgers', 'Aaron', '1983-12-02', 'Green Bay Packers', 'https://lasueur.com/wp-content/uploads/2017/12/Aaron-Rodgers.jpg', 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7827b2c1-5dcb-455b-8653-0e324f8adb1c/dcgae26-79376b0a-9b32-4050-a091-01743bbfa09b.png/v1/fill/w_1024,h_2190,strp/aaron_rodgers__green_bay_packers____nfl_by_nicolopez2602_dcgae26-fullview.png?token=', 'http://wallpapersexpert.com/data/out/16/3410459-aaron-rodgers-wallpapers.jpg', 11, 4),
+('Gronkowski', 'Rob', '1989-05-14', 'New England Patriots', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Rob_Gronkowski.JPG/800px-Rob_Gronkowski.JPG', 'http://www.aljanh.net/data/archive/img/4131712633.png', 'https://www.elsetge.cat/myimg/f/220-2206122_rob-gronkowski-patriots-wallpapers-top-free-rob-gronkowski.jpg', 12, 4),
+('Crosby', 'Sidney ', '1987-08-07', 'Pittsburgh Penguins', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Sidney_Crosby_1_2017-05-25_%28Edited%29.jpg/220px-Sidney_Crosby_1_2017-05-25_%28Edited%29.jpg', 'https://retohercules.com/images/crosby-png-16.png', 'https://i0.wp.com/instant.com.pk/wp-content/uploads/2018/11/sidney-crosby-111418-usnews-getty-ftr_a3eq7auh1fmh1d6ltpjdc5nry.jpg?fit=1920%2C1080&ssl=1', 13, 3),
+('Kane', 'Patrick', '1988-11-19', 'Chicago Blackhawks', 'https://img.s-msn.com/tenant/amp/entityid/BBgzGRT.img?x=1&y=1&f=PNG&h=336&w=320&m=5&q=60&b=white', 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/d92b8805-8cab-4cc6-9aa7-2d130577f990/d8063rz-823491b9-582b-4777-bbb8-e05fdc835591.png', 'http://www.swisshabs.ch/wp-content/uploads/2015/12/nhl_g_kane_kh_1920x1080.jpg', 14, 3),
+('Ovetchkine ', 'Aleksandr ', '1985-09-17', 'Washington Capitals', 'https://s.yimg.com/ny/api/res/1.2/gdhG3QtFD.rmlV24GkeIiw--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NzQ0O2g9NDk2/http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/bff536b0e7b7527d4f21b3ea4c4f10bb', 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d92b8805-8cab-4cc6-9aa7-2d130577f990/d82x4e0-5c7b9bf7-f316-45dd-a4e1-46cf0db1fdeb.png/v1/fill/w_1024,h_853,strp/alexander_ovechkin_png__1_by_meganl125_d82x4e0-fullview.png?token=eyJ0eXAiOiJKV1QiLCJ', 'https://www.pixelstalk.net/wp-content/uploads/2016/09/1920x1080-Alexander-Ovechkin-Background.jpg', 15, 3);
 
 -- --------------------------------------------------------
 
