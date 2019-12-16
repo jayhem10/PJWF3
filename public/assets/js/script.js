554f8c7fc2f2db29650ecf8077fea4b144db4a9b
ObdOJb <57894568+ObdOJb@users.noreply.github.com>
2019-12-16 11:55:31 +0100

    création view player

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,4 +3,7 @@ $(".full").hover(function(){
     }, function(){
     $(this).css("background-color", "purple");
   });
-  
+  $('#stat').tooltip({ 
+    boundary: 'window',
+    delay: { "show": 500, "hide": 4000 }
+ });
--- public/views/pages/player.php
+++ public/views/pages/player.php
@@ -4,10 +4,16 @@
 
 
   <div class="container">
-    <h1><?= $player['p_nom']?> <?= $player['p_prenom']?></h1>
+    <h1><?= $player['p_prenom']?> <?= $player['p_nom']?> </h1>
     </div>
 
-  <div class="container">
+    <div class="container">
+    <div class="row">
+		<div class="col-12">
+      <a id='stat' class="btn btn-secondary" target="_blank" data-toggle="tooltip" data-placement="top" href="https://www.wilson.com/fr-fr/football/footballs/nfl/nfl-duke/the-duke-nfl-football" title="">
+ Ballon du match
+</a>
+
 	  <div class="row">
 		  <div class="full">
 	      <img src="<?= $player['p_full']?>" class="img-fluid">
@@ -20,3 +26,4 @@
 
 
 <?php $content = ob_get_clean() ?> <?php view('template', compact('content')); ?>
+
