223f82a61b27e88584a679b954d02339342d6935
YassineFassi <yassine.fassi@gmail.com>
2019-12-16 15:41:05 +0100

    maj bdd + recherche joueur

--- public/views/pages/user.php
+++ public/views/pages/user.php
@@ -5,26 +5,22 @@
 
 <div class="container">
 
-<h2 class="text-warning py-5" >Bienvenue dans ton arène <b><?= $user['usr_prenom']; ?></b>!</h>
-
+<h2 class="text-warning py-5" >Bienvenue dans ton arène <b><?= $user['usr_prenom']; ?></b>!</h2><br>
+<h5 class="text-light pt-5">Tes sportifs préférés sont ici :</h5>
 </div> 
 
-
-
   <div class="container">
-    <h5 class="text-light pt-5">Tes sportifs préférés sont ici :</h5>
     <div class="row">
 
       <?php foreach( $joueur as $joueurs) : ?>
 
-
-          <div class="card-deck py-5 col-lg-4">
+          <div class="card-deck py-3 col-lg-4">
 
             <div class="card">
-                <img class="card-img-top" src="<?= $joueurs['p_img']; ?>" alt="Card image cap">
+                <img class="card-img-top" style="width : 318px; height : 300px" src="<?= $joueurs['p_img']; ?>" alt="Card image cap">
                 <div class="card-body">
-                  <h5 class="card-title"><?= $joueurs['p_nom']?> <?= $joueurs['p_prenom']?></h5>
-                  <p class="card-text">Club actuel : <?= $joueurs['p_club']?></p>
+                  <h5 class="card-title"><?= $joueurs['p_nom']?> <?= $joueurs['p_prenom']?> <img style ="width : 35px; float: right; margin-right: auto "  src="https://laendercode.net/img/flag-icon-css/flags/4x3/<?= $joueurs['code_pays'];?>.svg"/></h5>
+                  <p class="card-text">Club actuel : <?= $joueurs['p_club']?><img style ="width : 35px; float: right; margin-right: auto "  src="<?= $joueurs['code_club'];?>"/></p>
                   <p class="card-text">Né(e) le : <?= $joueurs['p_birth']?></p>
                   <a class="btn btn-success" href="<?=url('player/'.$joueurs['p_id']);?>" role="button">Consulter sa fiche</a>
                 </div>
@@ -37,6 +33,8 @@
  
     <a class="btn btn-warning center-block" href=" <?= url('allplayer')?>" role="button">Découvre les autres joueurs</a>
 
+    
+
 </div>
 </div>
 </div>
@@ -47,17 +45,14 @@
 <div class="container">
   <div class="row">
 
-  
 
-    <div><a class="col twitter-timeline" href="https://twitter.com/TrashTalk_fr?ref_src=twsrc%5Etfw">Tweets by TrashTalk_fr</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
 
-    <div><a class="col twitter-timeline" href="https://twitter.com/NBAFRANCE?ref_src=twsrc%5Etfw">Tweets by NBAFRANCE</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
+    <div><?=$user['twitter_1']; ?><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
+    <div><?=$user['twitter_2']; ?><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
+    <div><?=$user['twitter_3']; ?><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
 
-    <div><a class="col twitter-timeline" href="https://twitter.com/NBAextra?ref_src=twsrc%5Etfw">Tweets by NBAextra</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
-
-    <div><a class="col twitter-timeline" href="https://twitter.com/FantasyLabsNBA?ref_src=twsrc%5Etfw">Tweets by FantasyLabsNBA</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
-    
   </div>
+
 </div>
 
 <?php $content = ob_get_clean() ?> <?php view('template', compact('content')); ?>
