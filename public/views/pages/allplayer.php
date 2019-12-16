efbce93f3f2d0d47768eb0d1234866dce585d570
jayhem10 <57893801+jayhem10@users.noreply.github.com>
2019-12-16 11:44:18 +0100

    allplayer + user

--- public/views/pages/allplayer.php
+++ public/views/pages/allplayer.php
@@ -5,7 +5,7 @@
 <?php foreach($players as $listPlayers) : ?>
 <div class="container my-5">
     <div class="hover1">
-	    <a href="<?=url('player/'.$joueurs['p_id']);?>"><img src="<?= $listPlayers['p_img'];?>" class="rounded mx-auto d-block img-fluid" alt="Responsive image" style="width"><h2 class="titrelebron"><?= $listPlayers['p_prenom'];?> <?= $listPlayers['p_nom'];?></h2></a>
+	    <a href="<?=url('player/'.$joueurs['p_id']);?>"><img src="<?= $listPlayers['p_img'];?>" class="rounded mx-auto d-block img-fluid" alt="Responsive image" style="width"><h2 class="titrelebron"><?= $listPlayers['p_img_hovers'];?> <?= $listPlayers['p_nom'];?></h2></a>
     </div>
 </div>
 
