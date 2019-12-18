f8c741c9403d21ccbba92e3c0e97e879233a53f9
jayhem10 <57893801+jayhem10@users.noreply.github.com>
2019-12-18 11:31:48 +0100

    css

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,8 +1,4 @@
-// $(".full").hover(function(){
-//     $(this).css("background-color", "black");
-//     }, function(){
-//     $(this).css("background-color", "grey");
-//   });
+
 $('.stat').popover({ 
     boundary: 'window',
     trigger: 'focus'
