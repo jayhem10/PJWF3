e19a415439193ba92f1c473c07b898cebd34d5be
jayhem10 <57893801+jayhem10@users.noreply.github.com>
2019-12-19 10:31:56 +0100

    the final github ( pour de vrai )

--- public/views/template.php
+++ public/views/template.php
@@ -161,8 +161,8 @@
         <a class="social" href="#"><i class="fab fa-facebook-square display-3"></i></a>
         <!-- Twitter -->
         <a class="social" href="#"><i class="fab fa-twitter-square display-3"></i></a>
-        <!-- Google +-->
-        <a class="social" href="#"><i class="fab fa-google-plus-square display-3"></i></a>
+        <!-- Pinterest-->
+        <a class="social" href="#"><i class="fab fa-pinterest-square display-3"></i></a>
         <!-- Instagram -->
         <a class="social" href="#"><i class="fab fa-instagram display-3"></i></a>
 
@@ -170,9 +170,7 @@
 
 </footer>
       <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-  
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> 
       <script src="https://kit.fontawesome.com/bda8027245.js" crossorigin="anonymous"></script>
       <script src="<?= js_url('script.js'); ?>"></script>
